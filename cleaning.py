a1789d8a97eb2d64b8eae06b40944b630ad3343a
Paula0923 <paula.boks@gmail.com>
2024-09-28 17:25:35 +0300

    ...

--- cleaning.py
+++
@@ -1,8 +0,0 @@
-"""
-copied from cleaning lab:
-"Bonus: Challenge 2: creating functions on a separate py file¶
-Put all the data cleaning and formatting steps into functions, and create a main function that performs all the cleaning and formatting.
-
-Write these functions in separate .py file(s). By putting these steps into functions, we can make the code more modular and easier to maintain.
-
-Hint: autoreload module is a utility module in Python that allows you to automatically reload modules in the current session when changes are made to the source code. This can be useful in situations where you are actively developing code and want to see the effects of changes you make without having to constantly restart the Python interpreter or Jupyter Notebook kernel."
